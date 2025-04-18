2b1e3625fed191b9328206308e99c33105cead59
Anh Viet Ngo <anhviet2002.vna@gmail.com>
2025-04-18 18:42:59 +0200

    scrape races finished

--- f1_shared.py
+++ f1_shared.py
@@ -1,11 +1,9 @@
 from datetime import datetime
 import certifi
-import pandas as pd
 import aiohttp
 import ssl
 
 ssl_context = ssl.create_default_context(cafile=certifi.where())
-connector = aiohttp.TCPConnector(ssl=ssl_context)
 
 head = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
 base_url = "https://www.formula1.com"
@@ -14,13 +12,8 @@ base_url = "https://www.formula1.com"
 current_year = datetime.now().year
 years = [year for year in range(1950, current_year + 1)]
 
-# Helper function to save data to CSV
-def save_to_json(data, headers, filename):
-    df = pd.DataFrame(data, columns=headers)
-    df.to_json(filename, orient='records', lines=True)
-    print(df)
-
 async def test_function(param, functions):
+    connector = aiohttp.TCPConnector(ssl=ssl_context)
     async with aiohttp.ClientSession(connector=connector) as session:
         result = await functions(session, param)
         return result
